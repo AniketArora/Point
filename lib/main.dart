4fae1a69d5e4ee751282847b098a4862d4f797cc
Aniket Arora <arora.aniket@hotmail.com>
2020-01-03 22:19:26 +0100

    Made left side of mainpage

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Point',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: new Homepage(title: 'Homepage'));
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Point',
+      theme: ThemeData(
+        fontFamily: 'SF-Pro-Display',
+      ),
+      home: new Homepage(
+        title: 'Homepage',
+      ),
+    );
   }
 }
 
@@ -28,15 +31,126 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-        body: new Container(
-      margin: const EdgeInsets.all(16.0),
-      child: new Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          new Text('Point', style: Theme.of(context).textTheme.display1),
-        ],
+    return Material(
+      type: MaterialType.transparency,
+      child: new Container(
+        child: new Row(
+          children: <Widget>[
+            Expanded(
+              flex: 5,
+              child: new Container(
+                color: Color.fromRGBO(255, 255, 255, 1),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Container(
+                      child: Text(
+                        'Point',
+                        style: TextStyle(
+                          fontFamily: 'SF-Pro-Display',
+                          fontWeight: FontWeight.w600,
+                          fontSize: 48,
+                          color: Color.fromRGBO(0, 0, 0, 1),
+                        ),
+                      ),
+                      alignment: Alignment.topLeft,
+                      margin: EdgeInsets.fromLTRB(32, 32, 32, 16),
+                    ),
+                    Container(
+                      child: Text(
+                        'Verzamel Punten & Ontvang leuke prijzen!',
+                        style: TextStyle(
+                          fontFamily: 'SF-Pro-Display',
+                          fontWeight: FontWeight.w600,
+                          fontSize: 32,
+                          color: Color.fromRGBO(0, 0, 0, 1),
+                        ),
+                      ),
+                      alignment: Alignment.topLeft,
+                      margin: EdgeInsets.fromLTRB(32, 0, 32, 32),
+                    ),
+                    Container(
+                      child: ConstrainedBox(
+                        constraints:
+                            const BoxConstraints(minWidth: double.infinity),
+                        child: RaisedButton(
+                          onPressed: () {},
+                          color: Color.fromRGBO(8, 162, 254, 1),
+                          padding: EdgeInsets.fromLTRB(16, 16, 16, 16),
+                          child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              'Start',
+                              textAlign: TextAlign.start,
+                              style: TextStyle(
+                                fontFamily: 'SF-Pro-Display',
+                                fontWeight: FontWeight.w600,
+                                fontSize: 32,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      alignment: Alignment.bottomLeft,
+                      margin: EdgeInsets.fromLTRB(32, 336, 32, 16),
+                    ),
+                    Container(
+                      child: ConstrainedBox(
+                        constraints:
+                            const BoxConstraints(minWidth: double.infinity),
+                        child: OutlineButton(
+                          onPressed: () {},
+                          color: Colors.white,
+                          padding: EdgeInsets.fromLTRB(16, 16, 16, 16),
+                          highlightedBorderColor: Colors.grey,
+                          borderSide: BorderSide(
+                            color: Colors.black,
+                            style: BorderStyle.solid,
+                            width: 1,
+                          ),
+                          child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              'Check je puntensaldo',
+                              textAlign: TextAlign.start,
+                              style: TextStyle(
+                                fontFamily: 'SF-Pro-Display',
+                                fontWeight: FontWeight.w600,
+                                fontSize: 32,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      alignment: Alignment.bottomLeft,
+                      margin: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 5,
+              child: new Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    stops: [0.0, 1.0],
+                    colors: [
+                      Color.fromRGBO(120, 192, 233, 1),
+                      Color.fromRGBO(5, 161, 255, 1),
+                    ],
+                  ),
+                ),
+                child: new Container(),
+              ),
+            )
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
